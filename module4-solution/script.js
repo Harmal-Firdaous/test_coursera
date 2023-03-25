5094f3406f2bd86db8ab173300ce1bf21a3a7e3b
Harmal-Firdaous <127455706+Harmal-Firdaous@users.noreply.github.com>
2023-03-25 22:09:01 +0000

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -30,17 +30,17 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
-})();
+    var names = ["xaxa", "xixi", "xoxo", "xexe", "sasa", "soso", "sisi", "sese", "nana", "nono"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
